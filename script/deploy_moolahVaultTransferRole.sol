c04bfc9f19812942cfb7bce10bbe4540b050d151
rick <jing.xin@lista.org>
2025-04-07 14:46:27 +0800

    fea: update online address

--- script/deploy_moolahVaultTransferRole.sol
+++ script/deploy_moolahVaultTransferRole.sol
@@ -5,12 +5,12 @@ import "forge-std/Script.sol";
 import { MoolahVault } from "moolah-vault/MoolahVault.sol";
 
 contract MoolahVaultTransferRoleDeploy is Script {
-  // todo
+  // todo update vault and manager
   MoolahVault vault = MoolahVault(0xA5edCb7c60448f7779361afc2F92f858f3A6dd1E);
-  address admin = 0x05E3A7a66945ca9aF73f66660f22ffB36332FA54; // timelock
+  address admin = 0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253; // timelock
   address manager = 0x05E3A7a66945ca9aF73f66660f22ffB36332FA54; // timelock
-  address allocator = 0x05E3A7a66945ca9aF73f66660f22ffB36332FA54;
-  address curator = 0x05E3A7a66945ca9aF73f66660f22ffB36332FA54;
+  address allocator = 0x85CE862C5BB61938FFcc97DA4A80C8aaE43C6A27;
+  address curator = 0x8d388136d578dCD791D081c6042284CED6d9B0c6;
 
   bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
   bytes32 public constant MANAGER = keccak256("MANAGER");
