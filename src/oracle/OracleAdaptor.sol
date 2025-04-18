5d4e9644f134763dd4db757c659669c13c1b873e
Razorback <raz.w@binance.com>
2025-04-18 12:31:34 +0800

    feat: OracleAdaptor support PT oracles

--- src/oracle/OracleAdaptor.sol
+++ src/oracle/OracleAdaptor.sol
@@ -6,6 +6,7 @@ import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils
 
 import { IOracle, TokenConfig } from "../moolah/interfaces/IOracle.sol";
 import { IStakeManager } from "./interfaces/IStakeManager.sol";
+import { PTOracleType, PTOracleConfig, ILinearDiscountOracle } from "./interfaces/IPTOracle.sol";
 
 contract OracleAdaptor is AccessControlEnumerableUpgradeable, UUPSUpgradeable, IOracle {
   // @dev resilient oracle address
@@ -16,10 +17,16 @@ contract OracleAdaptor is AccessControlEnumerableUpgradeable, UUPSUpgradeable, I
   address public constant SLISBNB = 0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B;
   // @dev Stake Manager Address
   address public constant STAKE_MANAGER = 0x1adB950d8bB3dA4bE104211D5AB038628e477fE6;
-  // @dev asset mapping
+  // @dev asset mapping for resilient oracle
   mapping(address => address) public assetMap;
+  // @dev PT asset uses this oracle for price if set
+  // @dev PT asset => PT oracle address and type
+  mapping(address => PTOracleConfig) public ptOracles;
 
   event AssetMapUpdated(address indexed srcAsset, address indexed targetAsset);
+  event PTOracleUpdated(address indexed asset, PTOracleType oracleType, address oracleAddress);
+
+  bytes32 public constant MANAGER = keccak256("MANAGER");
 
   /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() {
@@ -45,6 +52,11 @@ contract OracleAdaptor is AccessControlEnumerableUpgradeable, UUPSUpgradeable, I
   }
 
   function peek(address asset) public view returns (uint256) {
+    // Check if the asset is a PT asset and use the configured oracle
+    if (ptOracles[asset].oracleAddress != address(0) && ptOracles[asset].oracleType != PTOracleType.NONE) {
+      return _peekPtOracle(asset);
+    }
+
     // Handle slisBNB
     if (asset == SLISBNB) {
       uint256 price = IOracle(RESILIENT_ORACLE).peek(WBNB);
@@ -62,7 +74,30 @@ contract OracleAdaptor is AccessControlEnumerableUpgradeable, UUPSUpgradeable, I
     }
   }
 
+  function _peekPtOracle(address asset) internal view returns (uint256) {
+    PTOracleConfig memory config = ptOracles[asset];
+
+    if (config.oracleType == PTOracleType.LINEAR_DISCOUNT) {
+      (uint80 _roundId, int256 answer, , , ) = ILinearDiscountOracle(config.oracleAddress).latestRoundData();
+      require(answer > 0, "OracleAdaptor: Invalid price");
+      return uint256(answer) / 1e10;
+    } else {
+      revert("OracleAdaptor: Unsupported oracle type");
+    }
+  }
+
   function getTokenConfig(address asset) external view override returns (TokenConfig memory) {
+    // If the asset is a PT asset, return the configured oracle
+    if (ptOracles[asset].oracleAddress != address(0)) {
+      return
+        TokenConfig({
+          asset: asset,
+          oracles: [address(this), address(0), address(0)],
+          enableFlagsForOracles: [true, false, false],
+          timeDeltaTolerance: 0
+        });
+    }
+
     address targetAsset = assetMap[asset];
 
     // Handle slisBNB
@@ -84,12 +119,42 @@ contract OracleAdaptor is AccessControlEnumerableUpgradeable, UUPSUpgradeable, I
   /// @dev only admin can update the asset mapping
   /// @param srcAsset source asset address
   /// @param targetAsset target asset address
-  function updateAssetMap(address srcAsset, address targetAsset) external onlyRole(DEFAULT_ADMIN_ROLE) {
+  function updateAssetMap(address srcAsset, address targetAsset) external onlyRole(MANAGER) {
     require(srcAsset != targetAsset, "OracleAdaptor: Invalid mapping");
     require(targetAsset != address(0), "OracleAdaptor: Target asset cannot be zero address");
     assetMap[srcAsset] = targetAsset;
 
     emit AssetMapUpdated(srcAsset, targetAsset);
+  }
+
+  /// @dev config PT oracle
+  /// @param asset PT asset address
+  /// @param oracleType oracle type: LINEAR_DISCOUNT or TWAP
+  /// @param oracleAddress oracle address
+  function configPtOracle(address asset, PTOracleType oracleType, address oracleAddress) external onlyRole(MANAGER) {
+    require(asset != address(0), "OracleAdaptor: Invalid asset");
+    require(oracleAddress != address(0), "OracleAdaptor: Invalid oracle address");
+    require(oracleType != PTOracleType.NONE, "OracleAdaptor: Invalid oracle type");
+
+    if (oracleType == PTOracleType.LINEAR_DISCOUNT) {
+      require(ILinearDiscountOracle(oracleAddress).PT() == asset, "OracleAdaptor: Asset mismatch");
+      require(ILinearDiscountOracle(oracleAddress).decimals() == 18, "OracleAdaptor: Invalid oracle decimals");
+
+      ptOracles[asset] = PTOracleConfig({ oracleType: oracleType, oracleAddress: oracleAddress });
+
+      emit PTOracleUpdated(asset, oracleType, oracleAddress);
+    } else {
+      revert("OracleAdaptor: Unsupported oracle type");
+    }
+  }
+
+  /// @dev remove PT oracle
+  /// @param asset PT asset address
+  function removePtOracle(address asset) external onlyRole(MANAGER) {
+    require(ptOracles[asset].oracleAddress != address(0), "OracleAdaptor: PT oracle not set");
+    delete ptOracles[asset];
+
+    emit PTOracleUpdated(asset, PTOracleType.NONE, address(0));
   }
 
   function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
---
+++ test/oracle/OracleAdaptorMainnet.t.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import { OracleAdaptor } from "../../src/oracle/OracleAdaptor.sol";
+import { ERC20Mock } from "../../src/moolah/mocks/ERC20Mock.sol";
+import { IStakeManager } from "../../src/oracle/interfaces/IStakeManager.sol";
+import { PTOracleType, PTOracleConfig, ILinearDiscountOracle } from "../../src/oracle/interfaces/IPTOracle.sol";
+import { IOracle } from "../../src/moolah/interfaces/IOracle.sol";
+
+interface IPTExpiry {
+  function expiry() external view returns (uint256);
+}
+
+contract OracleAdaptorTest is Test {
+  bytes32 private constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+  OracleAdaptor oracleAdaptor;
+
+  address proxyAddress = 0x21650E416dC6C89486B2E654c86cC2c36c597b58; // Mainnet OracleAdaptor
+  address WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+  address slisBNB = 0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B;
+  address ptClisBNB25apr = 0xE8F1C9804770e11Ab73395bE54686Ad656601E9e;
+  address ptSusde26Jun2025 = 0xDD809435ba6c9d6903730f923038801781cA66ce;
+  address ptSusde26Jun2025Oracle = 0x2AD358a2972aD56937A18b5D90A4F087C007D08d;
+
+  address admin = 0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253; // timelock
+  address manager = address(0x01);
+
+  function setUp() public {
+    vm.createSelectFork("https://bsc-dataseed.bnbchain.org");
+
+    // Test upgrade
+    OracleAdaptor impl = new OracleAdaptor();
+    address newImlp = address(impl);
+    address oldImpl = 0x274Cf42caf813537A81708E5a26B7c5760eDB517;
+    vm.startPrank(admin);
+    UUPSUpgradeable proxy = UUPSUpgradeable(proxyAddress);
+    assertEq(getImplementation(proxyAddress), oldImpl);
+    proxy.upgradeToAndCall(newImlp, bytes(""));
+    assertEq(getImplementation(proxyAddress), newImlp);
+    vm.stopPrank();
+
+    oracleAdaptor = OracleAdaptor(proxyAddress);
+
+    assertEq(oracleAdaptor.assetMap(address(ptClisBNB25apr)), WBNB);
+    assertEq(oracleAdaptor.assetMap(address(ptSusde26Jun2025)), address(0));
+    (PTOracleType type_, address oracle_) = oracleAdaptor.ptOracles(ptSusde26Jun2025);
+    assertEq(uint256(type_), 0); // NONE
+    assertEq(oracle_, address(0));
+
+    // Add manager
+    vm.prank(admin);
+    oracleAdaptor.grantRole(keccak256("MANAGER"), manager);
+    assertTrue(oracleAdaptor.hasRole(keccak256("MANAGER"), manager));
+  }
+
+  function test_peek_slisBnb() public {
+    vm.mockCall(
+      address(oracleAdaptor.RESILIENT_ORACLE()),
+      abi.encodeWithSelector(IOracle.peek.selector, oracleAdaptor.WBNB()),
+      abi.encode(uint256(62377900546)) // returns $623.77900546
+    );
+    vm.mockCall(
+      address(oracleAdaptor.STAKE_MANAGER()),
+      abi.encodeWithSelector(IStakeManager.convertSnBnbToBnb.selector, uint256(1e10)),
+      abi.encode(uint256(10270000000)) // 1.027
+    );
+    uint256 price = oracleAdaptor.peek(oracleAdaptor.SLISBNB());
+    uint256 expected = uint256(62377900546 * 10270000000) / 1e10;
+    assertEq(price, expected);
+  }
+
+  function test_peek_ptClisBnb() public {
+    vm.mockCall(
+      address(oracleAdaptor.RESILIENT_ORACLE()),
+      abi.encodeWithSelector(IOracle.peek.selector, WBNB),
+      abi.encode(uint256(62377900546)) // returns $623.77900546
+    );
+    uint256 price = oracleAdaptor.peek(WBNB);
+    assertEq(price, 62377900546);
+
+    price = oracleAdaptor.peek(address(ptClisBNB25apr));
+    assertEq(price, 62377900546);
+  }
+
+  function test_config_ptOracle() public {
+    vm.expectRevert();
+    oracleAdaptor.configPtOracle(ptSusde26Jun2025, PTOracleType.LINEAR_DISCOUNT, ptSusde26Jun2025Oracle);
+    vm.prank(manager);
+    oracleAdaptor.configPtOracle(ptSusde26Jun2025, PTOracleType.LINEAR_DISCOUNT, ptSusde26Jun2025Oracle);
+
+    (PTOracleType type_, address oracle_) = oracleAdaptor.ptOracles(ptSusde26Jun2025);
+    assertEq(uint256(type_), 1); // LINEAR_DISCOUNT
+    assertEq(oracle_, ptSusde26Jun2025Oracle);
+  }
+
+  function test_peek_ptSusde26Jun2025() public {
+    test_config_ptOracle();
+
+    uint256 price = oracleAdaptor.peek(ptSusde26Jun2025);
+    uint256 maturity = IPTExpiry(ptSusde26Jun2025).expiry();
+    uint256 timeLeft = (maturity > block.timestamp) ? maturity - block.timestamp : 0;
+    uint256 expected = 1e18 - ILinearDiscountOracle(ptSusde26Jun2025Oracle).getDiscount(timeLeft);
+
+    assertEq(price, expected / 1e10); // 1e8 is the decimal of the oracle
+  }
+
+  function test_remove_ptOracle() public {
+    vm.expectRevert();
+    oracleAdaptor.removePtOracle(ptSusde26Jun2025);
+    test_config_ptOracle();
+
+    vm.prank(manager);
+    oracleAdaptor.removePtOracle(ptSusde26Jun2025);
+
+    (PTOracleType type_, address oracle_) = oracleAdaptor.ptOracles(ptSusde26Jun2025);
+    assertEq(uint256(type_), 0); // NONE
+    assertEq(oracle_, address(0));
+  }
+
+  function test_updateAssetMap() public {
+    address newAsset = address(new ERC20Mock());
+    vm.expectRevert();
+    oracleAdaptor.updateAssetMap(address(ptClisBNB25apr), newAsset);
+    vm.prank(manager);
+    oracleAdaptor.updateAssetMap(address(ptClisBNB25apr), newAsset);
+    assertEq(oracleAdaptor.assetMap(address(ptClisBNB25apr)), newAsset);
+  }
+
+  function getImplementation(address _proxyAddress) public view returns (address) {
+    bytes32 implSlot = vm.load(_proxyAddress, IMPLEMENTATION_SLOT);
+    return address(uint160(uint256(implSlot)));
+  }
+}
