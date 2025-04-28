ece1c8cba0259e473b2b921df8a9d1809dad1bf8
Razorback <raz.w@binance.com>
2025-04-28 13:28:44 +0800

    fix: add loan asset to PT oracles

--- script/deploy_pTLinearDiscountOracle.sol
+++ script/deploy_pTLinearDiscountOracle.sol
@@ -21,11 +21,21 @@ contract PTLinearDiscountOracleDeploy is Script {
     address ptSusde26Jun2025Oracle = 0x2AD358a2972aD56937A18b5D90A4F087C007D08d;
     address admin = 0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253; // timelock
     address manager = 0x8d388136d578dCD791D081c6042284CED6d9B0c6;
+    address loanAsset = address(0); // USDe CA
+    address loanTokenOracle = address(0); // USDe oracle; multi oracle
 
     // Deploy OracleAdaptor proxy
     ERC1967Proxy proxy = new ERC1967Proxy(
       address(impl),
-      abi.encodeWithSelector(impl.initialize.selector, admin, manager, ptSusde26Jun2025, ptSusde26Jun2025Oracle)
+      abi.encodeWithSelector(
+        impl.initialize.selector,
+        admin,
+        manager,
+        ptSusde26Jun2025,
+        ptSusde26Jun2025Oracle,
+        loanAsset,
+        loanTokenOracle
+      )
     );
     console.log("PTLinearDiscountOracle proxy: ", address(proxy));
 
