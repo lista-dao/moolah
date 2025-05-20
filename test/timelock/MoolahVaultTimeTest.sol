b1ec8927f9aa50695110c02a0c4d9b40cdb0c712
rick <jing.xin@lista.org>
2025-05-20 17:13:49 +0800

    fix

--- test/timelock/MoolahVaultTimeTest.sol
+++ test/timelock/MoolahVaultTimeTest.sol
@@ -7,38 +7,50 @@ import { Test, console } from "forge-std/Test.sol";
 
 import { TimeLock } from "timelock/TimeLock.sol";
 import { MoolahVault } from "moolah-vault/MoolahVault.sol";
+import { Id } from "moolah/interfaces/IMoolah.sol";
 
 contract MoolahVaultTimeLockTest is Test {
   TimeLock timeLock = TimeLock(payable(0x2e2807F88C381Cb0CC55c808a751fC1E3fcCbb85));
-  MoolahVault vault = MoolahVault(0x57134a64B7cD9F9eb72F8255A671F5Bf2fe3E2d0);
+  MoolahVault vault = MoolahVault(0x6d6783C146F2B0B2774C1725297f1845dc502525);
   address proposer = 0x8d388136d578dCD791D081c6042284CED6d9B0c6;
   bytes32 constant CURATOR = 0x0aec9b08bc8c2cb62a91f52d33e3d77da4b3f3a63fc8b542a93abe3902ba929c;
+  address allocator = 0x85CE862C5BB61938FFcc97DA4A80C8aaE43C6A27;
 
   function setUp() public {
     vm.createSelectFork("bsc");
   }
 
-  function test_transferRole() public {
-    address[] memory targets = new address[](2);
+  function test_removeMarket() public {
+    address[] memory targets = new address[](3);
     targets[0] = address(vault);
     targets[1] = address(vault);
+    targets[2] = address(vault);
 
-    uint256[] memory values = new uint256[](2);
+    uint256[] memory values = new uint256[](3);
     values[0] = 0;
     values[1] = 0;
+    values[2] = 0;
 
-    bytes[] memory payloads = new bytes[](2);
-    payloads[0] = hex"2f2ff15d0aec9b08bc8c2cb62a91f52d33e3d77da4b3f3a63fc8b542a93abe3902ba929c0000000000000000000000002e2807f88c381cb0cc55c808a751fc1e3fccbb85";
-    payloads[1] = hex"d547741f0aec9b08bc8c2cb62a91f52d33e3d77da4b3f3a63fc8b542a93abe3902ba929c0000000000000000000000008d388136d578dcd791d081c6042284ced6d9b0c6";
+    bytes[] memory payloads = new bytes[](3);
+    payloads[0] = hex"d3ac290c00000000000000000000000055d398326f99059ff775485246999027b3197955000000000000000000000000dd809435ba6c9d6903730f923038801781ca66ce00000000000000000000000089852c82e4a7aa41c7691b374d5d5ef8487ec370000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c0000000000000000000000000000000000000000000000000cb2bba6f17b80000000000000000000000000000000000000000000000000000000000000000000";
+    payloads[1] = hex"41c68e4800000000000000000000000055d398326f99059ff775485246999027b3197955000000000000000000000000dd809435ba6c9d6903730f923038801781ca66ce00000000000000000000000089852c82e4a7aa41c7691b374d5d5ef8487ec370000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c0000000000000000000000000000000000000000000000000cb2bba6f17b8000";
+    payloads[2] = hex"d3ac290c00000000000000000000000055d398326f99059ff775485246999027b3197955000000000000000000000000dd809435ba6c9d6903730f923038801781ca66ce0000000000000000000000006961fe6bb5292279bbe72c7acfb9fdf7d3fbed13000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c0000000000000000000000000000000000000000000000000cb2bba6f17b8000000000000000000000000000000000000000000000108b2a2c28029094000000";
 
     bytes32 predecessor = 0x0000000000000000000000000000000000000000000000000000000000000000;
-    bytes32 salt = 0x0000000000000000000000000000000000000000000000000000000000000000;
+    bytes32 salt = 0x0000000000000000000000000000000000000000000000000000000000000009;
     uint256 delay = 86400;
 
-    address[] memory curators = vault.getRoleMembers(CURATOR);
-    for (uint256 i = 0; i < curators.length; i++) {
-      console.log(curators[i]);
-    }
+
+    Id oldId = Id.wrap(hex"b9db2f7e903ccab0b7f715500f925d3e3def42a364a5ebe6af52f4b10917bb03");
+    Id newId = Id.wrap(hex"417eef8a15b54c61c64026d13ff067611579d95d392c969cac919115b5a379a2");
+
+    (uint184 oldCap,bool oldEnabled,) = vault.config(oldId);
+    (uint184 newCap,bool newEnabled,) = vault.config(newId);
+
+    console.log("old", oldEnabled);
+    console.log("new", newEnabled);
+    console.log("oldCap", oldCap);
+    console.log("newCap", newCap);
 
     vm.startPrank(proposer);
     timeLock.scheduleBatch(targets, values, payloads, predecessor, salt, delay);
@@ -50,65 +62,52 @@ contract MoolahVaultTimeLockTest is Test {
     timeLock.executeBatch(targets, values, payloads, predecessor, salt);
     vm.stopPrank();
 
-    console.log("transfer role done!");
+    vm.startPrank(allocator);
+    removeMarket(oldId);
+    vm.stopPrank();
 
-    curators = vault.getRoleMembers(CURATOR);
-    for (uint256 i = 0; i < curators.length; i++) {
-      console.log(curators[i]);
+    console.log("execute done!");
+
+    (oldCap,oldEnabled,) = vault.config(oldId);
+    (newCap,newEnabled,) = vault.config(newId);
+
+    console.log("old", oldEnabled);
+    console.log("new", newEnabled);
+    console.log("oldCap", oldCap);
+    console.log("newCap", newCap);
+
+    for (uint256 i = 0; i < vault.supplyQueueLength(); i++) {
+      console.logBytes32(Id.unwrap(vault.supplyQueue(i)));
     }
   }
 
-  function test_setFeeRecipient() public {
-    address target = 0x57134a64B7cD9F9eb72F8255A671F5Bf2fe3E2d0;
+  function removeMarket(Id marketId) internal {
+    uint256 oldLen = vault.withdrawQueueLength();
+    uint256 newLen = oldLen - 1;
+    uint256[] memory indexes = new uint256[](newLen);
+    uint256 removeIndex = 0;
 
-    bytes memory data = hex"e74b981b00000000000000000000000044dc4cc17081b05a50aa970ed8ddd6c047bd549b";
+    for ((uint256 i, uint256 j) = (0, 0); i < oldLen; i++) {
+      if (Id.unwrap(vault.withdrawQueue(i)) != Id.unwrap(marketId)) {
+        indexes[j] = i;
+        j++;
+      } else {
+        removeIndex = i;
+      }
+    }
 
-    bytes32 predecessor = 0x0000000000000000000000000000000000000000000000000000000000000000;
-    bytes32 salt = 0x0000000000000000000000000000000000000000000000000000000000000001;
-    uint256 delay = 86400;
-
-    vm.startPrank(proposer);
-    timeLock.schedule(target, 0, data, predecessor, salt, delay);
-    vm.stopPrank();
-
-    skip(delay);
-
-    vm.startPrank(proposer);
-    timeLock.execute(target, 0, data, predecessor, salt);
-    vm.stopPrank();
-
-    console.log("set fee recipient done!");
-
-    address feeRecipient = vault.feeRecipient();
-    console.log("feeRecipient", feeRecipient);
-  }
-
-  function test_setCap() public {
-    address[] memory targets = new address[](4);
-    targets[0] = 0x57134a64B7cD9F9eb72F8255A671F5Bf2fe3E2d0;
-    targets[1] = 0x57134a64B7cD9F9eb72F8255A671F5Bf2fe3E2d0;
-    targets[2] = 0xfa27f172e0b6ebcEF9c51ABf817E2cb142FbE627;
-    targets[3] = 0xfa27f172e0b6ebcEF9c51ABf817E2cb142FbE627;
-    uint256[] memory values = new uint256[](4);
-    values[0] = 0;
-    values[1] = 0;
-    values[2] = 0;
-    values[3] = 0;
-
-    bytes[] memory payloads = new bytes[](4);
-    payloads[0] = hex"d3ac290c000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c000000000000000000000000b0b84d294e0c75a6abe60171b70edeb2efd14a1b00000000000000000000000021650e416dc6c89486b2e654c86cc2c36c597b58000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c0000000000000000000000000000000000000000000000000d645e63204080000000000000000000000000000000000000000000000069e10de76676d0800000";
-    payloads[1] = hex"d3ac290c000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c0000000000000000000000002170ed0880ac9a755fd29b2688956bd959f933f8000000000000000000000000f3afd82a4071f272f403dc176916141f44e6c750000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c0000000000000000000000000000000000000000000000000b1a2bc2ec500000000000000000000000000000000000000000000000001fc3842bd1f071c00000";
-    payloads[2] = hex"d3ac290c0000000000000000000000008d0d000ee44948fc98c9b98a4fa4921476f08b0d000000000000000000000000b0b84d294e0c75a6abe60171b70edeb2efd14a1b00000000000000000000000021650e416dc6c89486b2e654c86cc2c36c597b58000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c00000000000000000000000000000000000000000000000009b6e64a8ec60000000000000000000000000000000000000000000000295be96e64066972000000";
-    payloads[3] = hex"d3ac290c0000000000000000000000008d0d000ee44948fc98c9b98a4fa4921476f08b0d0000000000000000000000002170ed0880ac9a755fd29b2688956bd959f933f8000000000000000000000000f3afd82a4071f272f403dc176916141f44e6c750000000000000000000000000fe7dae87ebb11a7beb9f534bb23267992d9cde7c00000000000000000000000000000000000000000000000009b6e64a8ec60000000000000000000000000000000000000000000000295be96e64066972000000";
-
-    bytes32 predecessor = 0x0000000000000000000000000000000000000000000000000000000000000000;
-    bytes32 salt = 0x0000000000000000000000000000000000000000000000000000000000000003;
+    console.log("removeIndex", removeIndex);
 
 
-    vm.startPrank(proposer);
+    vault.updateWithdrawQueue(indexes);
 
-    timeLock.executeBatch(targets, values, payloads, predecessor, salt);
-    vm.stopPrank();
+    for (uint256 i; i < indexes.length; i++) {
+      console.log(indexes[i]);
+    }
+
+    console.log("supply queue length", vault.supplyQueueLength());
+    console.logBytes32(Id.unwrap(vault.supplyQueue(4)));
+
   }
 
 }
