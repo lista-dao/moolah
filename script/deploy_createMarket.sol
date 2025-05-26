3cfac169f5f90b7362b1a98bdc9c624cb43442ed
rick <jing.xin@lista.org>
2025-05-26 14:21:32 +0800

    fea: add usde susde marke

--- script/deploy_createMarket.sol
+++ script/deploy_createMarket.sol
@@ -23,6 +23,8 @@ contract CreateMarketDeploy is Script {
   address asUSDF = 0x917AF46B3C3c6e1Bb7286B9F59637Fb7C65851Fb;
   address wstETH = 0x26c5e01524d2E6280A48F2c50fF6De7e52E9611C;
   address wBETH = 0xa2E3356610840701BDf5611a53974510Ae27E2e1;
+  address USDe = 0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34;
+  address sUSDe = 0x211Cc4DD073734dA055fbF44a2b4667d5E5fE5d2;
 
   address multiOracle = 0xf3afD82A4071f272F403dC176916141f44E6c750;
   address oracleAdapter = 0x21650E416dC6C89486B2E654c86cC2c36c597b58;
@@ -43,30 +45,48 @@ contract CreateMarketDeploy is Script {
     address deployer = vm.addr(deployerPrivateKey);
     console.log("Deployer: ", deployer);
 
-    MarketParams[] memory params = new MarketParams[](8);
-    params[0] = MarketParams({ loanToken: WBNB, collateralToken: wBETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[1] = MarketParams({ loanToken: BTCB, collateralToken: wBETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[2] = MarketParams({ loanToken: USDT, collateralToken: wBETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[3] = MarketParams({ loanToken: USD1, collateralToken: wBETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[4] = MarketParams({ loanToken: WBNB, collateralToken: wstETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[5] = MarketParams({ loanToken: BTCB, collateralToken: wstETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[6] = MarketParams({ loanToken: USDT, collateralToken: wstETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
-    params[7] = MarketParams({ loanToken: USD1, collateralToken: wstETH, oracle: multiOracle, irm: irm, lltv: lltv80 });
+    MarketParams[] memory params = new MarketParams[](4);
+    params[0] = MarketParams({
+      loanToken: USD1,
+      collateralToken: USDe,
+      oracle: multiOracle,
+      irm: irm,
+      lltv: lltv915
+    });
+    params[1] = MarketParams({
+      loanToken: USD1,
+      collateralToken: sUSDe,
+      oracle: multiOracle,
+      irm: irm,
+      lltv: lltv915
+    });
+    params[2] = MarketParams({
+      loanToken: USDT,
+      collateralToken: USDe,
+      oracle: multiOracle,
+      irm: irm,
+      lltv: lltv915
+    });
+    params[3] = MarketParams({
+      loanToken: USDT,
+      collateralToken: sUSDe,
+      oracle: multiOracle,
+      irm: irm,
+      lltv: lltv915
+    });
 
     vm.startBroadcast(deployerPrivateKey);
-    for (uint256 i = 0; i < 8; i++) {
+    for (uint256 i = 0; i < 4; i++) {
       Id id = params[i].id();
       console.log("market id:");
       console.logBytes32(Id.unwrap(id));
       // check if market already exists
-      (, , , , uint128 lastUpdate, ) = moolah.market(id);
+      (,,,,uint128 lastUpdate,) = moolah.market(id);
       if (lastUpdate != 0) {
-        console.log("market already exists");
         continue;
       }
       // create market
       moolah.createMarket(params[i]);
-      console.log("market created");
     }
 
     vm.stopBroadcast();
