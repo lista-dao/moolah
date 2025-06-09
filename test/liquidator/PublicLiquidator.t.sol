2ed88d7647aa5659802f4c28ab470872d3783f22
Lawson Cheng <lawson.c@binance.com>
2025-06-09 17:02:33 +0800

    ! enhanced unit test for PublicLiquidator

--- test/liquidator/PublicLiquidator.t.sol
+++ test/liquidator/PublicLiquidator.t.sol
@@ -32,6 +32,7 @@ contract PublicLiquidatorTest is BaseTest {
     publicLiquidator = IPublicLiquidator(address(proxy));
   }
 
+  /// @dev whitelist market couldn't be added to the whitelist again
   function testDuplicateWhitelist() public {
     vm.startPrank(BOT);
     vm.expectRevert("market is already open for liquidate");
@@ -39,6 +40,7 @@ contract PublicLiquidatorTest is BaseTest {
     vm.stopPrank();
   }
 
+  /// @dev using loan token to bid on collateral token
   function testLiquidate() public {
     uint256 loanAmount = 1e19;
     uint256 collateralAmount = 1e19;
@@ -68,6 +70,7 @@ contract PublicLiquidatorTest is BaseTest {
     assertEq(collateralToken.balanceOf(USER), collateralAmount, "collateralToken balance");
   }
 
+  /// @dev flashloan loan token and earn the difference after sold the collateral token
   function testFlashLiquidate() public {
     uint256 loanAmount = 1e19;
     uint256 collateralAmount = 1e19;
@@ -103,5 +106,38 @@ contract PublicLiquidatorTest is BaseTest {
 
     assertEq(collateralToken.balanceOf(address(USER)), 0, "collateralToken balance");
     assertGt(loanToken.balanceOf(address(USER)), 0, "loanToken balance");
+  }
+
+  /// @dev revert when trying to liquidate a market that is not whitelisted
+  function testNonWhitelistLiquidate() public {
+    uint256 loanAmount = 1e19;
+    uint256 collateralAmount = 1e19;
+
+    oracle.setPrice(address(collateralToken), ORACLE_PRICE_SCALE);
+    oracle.setPrice(address(loanToken), ORACLE_PRICE_SCALE);
+
+    loanToken.setBalance(address(this), loanAmount);
+    loanToken.setBalance(address(publicLiquidator), loanAmount);
+    collateralToken.setBalance(address(this), collateralAmount);
+    moolah.supply(marketParams, loanAmount, 0, address(this), "");
+
+    moolah.supplyCollateral(marketParams, collateralAmount, address(this), "");
+
+    moolah.borrow(marketParams, 8e18, 0, address(this), address(this));
+
+    oracle.setPrice(address(collateralToken), ORACLE_PRICE_SCALE / 10);
+
+    // make this market only whitelisted address can liquidate
+    vm.prank(OWNER);
+    moolah.addLiquidationWhitelist(marketParams.id(), makeAddr("WHITELISTOR"));
+
+    vm.startPrank(USER);
+    // give user some loan token to buy collateral token
+    loanToken.setBalance(USER, 8e18 * 1.1);
+    // approve publicLiquidator to spend USER's loan token
+    loanToken.approve(address(publicLiquidator), 8e18 * 1.1);
+    vm.expectRevert(bytes4(keccak256("NotWhitelisted()")));
+    publicLiquidator.liquidate(Id.unwrap(marketParams.id()), address(this), collateralAmount, 0);
+    vm.stopPrank();
   }
 }
