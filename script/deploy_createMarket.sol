1052bfc742fa2477b000dff38e656eb7d4bb5413
rick <jing.xin@lista.org>
2025-06-10 09:55:15 +0800

    fea: add create new market

--- script/deploy_createMarket.sol
+++ script/deploy_createMarket.sol
@@ -29,6 +29,7 @@ contract CreateMarketDeploy is Script {
   address Puffer = 0x87d00066cf131ff54B72B134a217D5401E5392b6;
   address USDX = 0xf3527ef8dE265eAa3716FB312c12847bFBA66Cef;
   address sUSDX = 0x7788A3538C5fc7F9c7C8A74EAC4c898fC8d87d92;
+  address USR = 0x2492D0006411Af6C8bbb1c8afc1B0197350a79e9;
 
   address multiOracle = 0xf3afD82A4071f272F403dC176916141f44E6c750;
   address oracleAdapter = 0x21650E416dC6C89486B2E654c86cC2c36c597b58;
@@ -44,6 +45,7 @@ contract CreateMarketDeploy is Script {
   uint256 lltv75 = 75 * 1e16;
   uint256 lltv80 = 80 * 1e16;
   uint256 lltv85 = 85 * 1e16;
+  uint256 lltv86 = 86 * 1e16;
   uint256 lltv865 = 865 * 1e15;
   uint256 lltv90 = 90 * 1e16;
   uint256 lltv915 = 915 * 1e15;
@@ -54,14 +56,13 @@ contract CreateMarketDeploy is Script {
     address deployer = vm.addr(deployerPrivateKey);
     console.log("Deployer: ", deployer);
 
-    MarketParams[] memory params = new MarketParams[](4);
-    params[0] = MarketParams({ loanToken: USD1, collateralToken: USDX, oracle: multiOracle, irm: irm, lltv: lltv865 });
-    params[1] = MarketParams({ loanToken: USDT, collateralToken: sUSDX, oracle: multiOracle, irm: irm, lltv: lltv865 });
-    params[2] = MarketParams({ loanToken: USD1, collateralToken: sUSDe, oracle: multiOracle, irm: irm, lltv: lltv865 });
-    params[3] = MarketParams({ loanToken: USDT, collateralToken: sUSDe, oracle: multiOracle, irm: irm, lltv: lltv865 });
+    MarketParams[] memory params = new MarketParams[](3);
+    params[0] = MarketParams({ loanToken: USD1, collateralToken: USR, oracle: multiOracle, irm: irm, lltv: lltv86 });
+    params[1] = MarketParams({ loanToken: USDT, collateralToken: USR, oracle: multiOracle, irm: irm, lltv: lltv86 });
+    params[2] = MarketParams({ loanToken: USD1, collateralToken: solvBTC, oracle: multiOracle, irm: irm, lltv: lltv70 });
 
     vm.startBroadcast(deployerPrivateKey);
-    for (uint256 i = 0; i < 4; i++) {
+    for (uint256 i = 0; i < 3; i++) {
       Id id = params[i].id();
       console.log("market id:");
       console.logBytes32(Id.unwrap(id));
