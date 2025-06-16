e47c04e8be944777c5fcc48977d54e3819477ed8
Razorback <raz.w@binance.com>
2025-06-16 16:50:28 +0800

    feat: BNB provider script for loop wbnb vault

--- script/deploy_bnbProvider.sol
+++ script/deploy_bnbProvider.sol
@@ -6,10 +6,11 @@ import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy
 
 import { BNBProvider } from "../src/provider/BNBProvider.sol";
 
-contract MoolahVaultDeploy is Script {
+contract BNBProviderDeploy is Script {
   address moolah = 0x8F73b65B4caAf64FBA2aF91cC5D4a2A1318E5D8C;
   address vault = 0x57134a64B7cD9F9eb72F8255A671F5Bf2fe3E2d0; // Lista WBNB Vault
   address mevVault = 0xd5cfc0f894bA77e95E3325Aa53Eb3e6CBBb5A81E; // MEV WBNB Vault
+  address loopVault = vault; // Loop WBNB Vault
 
   address asset = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c; // WBNB
 
@@ -20,24 +21,15 @@ contract MoolahVaultDeploy is Script {
     vm.startBroadcast(deployerPrivateKey);
 
     // Deploy BNBProvider implementation
-    BNBProvider impl = new BNBProvider(moolah, vault, asset);
-    console.log("Lista WBNB Vault BNBProvider implementation: ", address(impl));
+    BNBProvider impl = new BNBProvider(moolah, loopVault, asset);
+    console.log("Loop WBNB Vault BNBProvider implementation: ", address(impl));
 
-    // Deploy Lista WBNB Vault BNBProvider proxy
+    // Deploy Loop WBNB Vault BNBProvider proxy
     ERC1967Proxy proxy = new ERC1967Proxy(
       address(impl),
       abi.encodeWithSelector(impl.initialize.selector, deployer, deployer)
     );
-    console.log("Lista WBNB Vault BNBProvider proxy: ", address(proxy));
-
-    // Deploy MEV WBNB Vault BNBProvider proxy
-    BNBProvider impl2 = new BNBProvider(moolah, mevVault, asset);
-    console.log("MEV WBNB Vault BNBProvider implementation: ", address(impl2));
-    ERC1967Proxy proxy2 = new ERC1967Proxy(
-      address(impl2),
-      abi.encodeWithSelector(impl2.initialize.selector, deployer, deployer)
-    );
-    console.log("MEV WBNB Vault BNBProvider proxy: ", address(proxy2));
+    console.log("Loop WBNB Vault BNBProvider proxy: ", address(proxy));
 
     vm.stopBroadcast();
   }
