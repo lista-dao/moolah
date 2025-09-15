bbab696548c3217dd2281151501acf58f79930e3
Lawson Cheng <lawson.c@binance.com>
2025-09-15 10:12:35 +0800

    ! _getPrice() removed redundant oracle price fetches oracle prices

--- src/moolah/libraries/PriceLib.sol
+++ src/moolah/libraries/PriceLib.sol
@@ -7,6 +7,17 @@ import { IBrokerBase } from "../../broker/interfaces/IBroker.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 library PriceLib {
+
+  /// @dev Returns the price of the collateral asset in terms of the loan asset
+  /// @notice if there is a broker for the market and user address is non-zero
+  ///         will return a price which might deviates from the market price according to user's position
+  /// @param marketParams The market parameters
+  /// @param user The user address
+  /// @param broker The broker address
+  /// @return basePrice The price of the collateral asset
+  /// @return quotePrice The price of the loan asset
+  /// @return baseTokenDecimals The decimals of the collateral asset
+  /// @return quoteTokenDecimals The decimals of the loan asset
   function _getPrice(
     MarketParams memory marketParams,
     address user,
@@ -15,11 +26,11 @@ library PriceLib {
     uint256 basePrice,
     uint256 quotePrice,
     uint256 baseTokenDecimals,
-    uint256 quotaTokenDecimals
+    uint256 quoteTokenDecimals
 ) {
     IOracle _oracle = IOracle(marketParams.oracle);
     baseTokenDecimals = IERC20Metadata(marketParams.collateralToken).decimals();
-    quotaTokenDecimals = IERC20Metadata(marketParams.loanToken).decimals();
+    quoteTokenDecimals = IERC20Metadata(marketParams.loanToken).decimals();
     basePrice = _oracle.peek(marketParams.collateralToken);
     quotePrice = _oracle.peek(marketParams.loanToken);
 
@@ -30,9 +41,6 @@ library PriceLib {
       IBrokerBase _broker = IBrokerBase(broker);
       basePrice = _broker.peek(marketParams.collateralToken, user);
       quotePrice = _broker.peek(marketParams.loanToken, user);
-    } else {
-      basePrice = _oracle.peek(marketParams.collateralToken);
-      quotePrice = _oracle.peek(marketParams.loanToken);
     }
    }
 }
