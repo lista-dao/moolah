c0fd98c9180b6f25484e6e92f1ccd6bb006f5610
Razorback <raz.w@binance.com>
2025-09-17 14:28:18 +0800

    feat: add cap for irm borrow rate

--- src/interest-rate-model/interfaces/IInterestRateModel.sol
+++ src/interest-rate-model/interfaces/IInterestRateModel.sol
@@ -14,4 +14,16 @@ interface IInterestRateModel is IIrm {
   /// @notice Rate at target utilization.
   /// @dev Tells the height of the curve.
   function rateAtTarget(Id id) external view returns (int256);
+
+  /// @notice Rate cap for the given market.
+  function rateCap(Id id) external view returns (uint256);
+
+  /// @notice Minimum borrow rate cap for all markets.
+  function minCap() external view returns (uint256);
+
+  /// @notice Updates the borrow rate cap for a market.
+  function updateRateCap(Id id, uint256 newRateCap) external;
+
+  /// @notice Updates the minimum borrow rate cap for all markets.
+  function updateMinCap(uint256 newMinCap) external;
 }
