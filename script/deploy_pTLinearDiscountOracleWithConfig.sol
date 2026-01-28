1a46674a6b080e6a4d685b1aeab870ab6fa53c4c
rick <jing.xin@lista.org>
2026-01-28 16:24:46 +0800

    fea: add PTLinearDiscountOracleWithConfig for dpeloying pt oracle

---
+++ script/deploy_pTLinearDiscountOracleWithConfig.sol
@@ -0,0 +1,97 @@
+pragma solidity 0.8.28;
+
+import "forge-std/Script.sol";
+import { Config } from "forge-std/Config.sol";
+
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+import { PTLinearDiscountOracle } from "../src/oracle/PTLinearDiscountOracle.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract PTLinearDiscountOracleWithConfigDeploy is Script, Config {
+  function run() public {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address deployer = vm.addr(deployerPrivateKey);
+    console.log("Deployer: ", deployer);
+
+    _loadMarketParams();
+
+    address[] memory loans = config.get("ptLoan").toAddressArray();
+    address[] memory collaterals = config.get("ptCollateral").toAddressArray();
+    address[] memory oracles = config.get("ptOracle").toAddressArray();
+    address multiOracle = config.get("multiOracle").toAddress();
+    address admin = config.get("admin").toAddress();
+    vm.startBroadcast(deployerPrivateKey);
+
+    for (uint256 i = 0; i < loans.length; i++) {
+      console.log(
+        "Deploying PTLinearDiscountOracle for loan:",
+        IERC20Metadata(loans[i]).symbol(),
+        " and collateral:",
+        IERC20Metadata(collaterals[i]).symbol()
+      );
+      deploy_PTOracle(admin, collaterals[i], loans[i], oracles[i], multiOracle);
+    }
+
+    vm.stopBroadcast();
+  }
+
+  function deploy_PTOracle(
+    address admin,
+    address collateral,
+    address loan,
+    address collateralOracle,
+    address loanOracle
+  ) public {
+    // Deploy implementation
+    PTLinearDiscountOracle impl = new PTLinearDiscountOracle();
+    console.log("PTLinearDiscountOracle implementation: ", address(impl));
+    // Deploy OracleAdaptor proxy
+    ERC1967Proxy proxy = new ERC1967Proxy(
+      address(impl),
+      abi.encodeWithSelector(impl.initialize.selector, admin, collateral, collateralOracle, loan, loanOracle)
+    );
+    console.log("PTLinearDiscountOracle proxy: ", address(proxy));
+  }
+
+  function _loadMarketParams() private {
+    _loadConfig("./config/params.toml", true);
+    string[] memory tokenNames = config.get("tokenNames").toStringArray();
+    address[] memory tokens = config.get("tokens").toAddressArray();
+    for (uint256 i = 0; i < tokenNames.length; i++) {
+      vm.setEnv(tokenNames[i], vm.toString(tokens[i]));
+    }
+
+    string[] memory oracleNames = config.get("oracleNames").toStringArray();
+    address[] memory oracles = config.get("oracles").toAddressArray();
+    for (uint256 i = 0; i < oracleNames.length; i++) {
+      vm.setEnv(oracleNames[i], vm.toString(oracles[i]));
+    }
+
+    string[] memory irmNames = config.get("irmNames").toStringArray();
+    address[] memory irms = config.get("irms").toAddressArray();
+    for (uint256 i = 0; i < irmNames.length; i++) {
+      vm.setEnv(irmNames[i], vm.toString(irms[i]));
+    }
+
+    string[] memory lltvNames = config.get("lltvNames").toStringArray();
+    uint256[] memory lltvs = config.get("lltvs").toUint256Array();
+    for (uint256 i = 0; i < lltvNames.length; i++) {
+      vm.setEnv(lltvNames[i], vm.toString(lltvs[i]));
+    }
+
+    string[] memory ptBaseOracleNames = config.get("ptBaseOracleNames").toStringArray();
+    address[] memory ptBaseOracles = config.get("ptBaseOracles").toAddressArray();
+    for (uint256 i = 0; i < ptBaseOracleNames.length; i++) {
+      vm.setEnv(ptBaseOracleNames[i], vm.toString(ptBaseOracles[i]));
+    }
+
+    string[] memory walletNames = config.get("walletNames").toStringArray();
+    address[] memory wallets = config.get("wallets").toAddressArray();
+    for (uint256 i = 0; i < walletNames.length; i++) {
+      vm.setEnv(walletNames[i], vm.toString(wallets[i]));
+    }
+
+    _loadConfig("./config/markets.toml", true);
+  }
+}
