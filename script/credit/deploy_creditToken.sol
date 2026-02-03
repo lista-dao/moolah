546f610bdfcc407ea81cfc81eba70cbb2c6cd376
Razorback <raz.w@binance.com>
2026-02-03 16:08:37 +0800

    chore: change address to mainnet address

--- script/credit/deploy_creditToken.sol
+++ script/credit/deploy_creditToken.sol
@@ -6,14 +6,13 @@ import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy
 import { CreditToken } from "src/utils/CreditToken.sol";
 
 contract CreditTokenDeploy is Script {
-  address moolah_testnet = 0x4c26397D4ef9EEae55735a1631e69Da965eBC41A;
   address moolah = 0x8F73b65B4caAf64FBA2aF91cC5D4a2A1318E5D8C;
   address bot = 0x6dD696c8DBa8764D0e5fD914A470FD5e780D0D12;
 
   bytes32 public constant BOT = keccak256("BOT");
 
   function run() public {
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_TESTNET");
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     address deployer = vm.addr(deployerPrivateKey);
     console.log("Deployer: ", deployer);
     vm.startBroadcast(deployerPrivateKey);
@@ -30,9 +29,10 @@ contract CreditTokenDeploy is Script {
       address(impl),
       abi.encodeWithSelector(
         impl.initialize.selector,
-        deployer,
-        deployer,
-        deployer,
+        deployer, // admin
+        deployer, // manager
+        deployer, // bot
+        deployer, // pauser
         transferers,
         "Lista Credit Token",
         "lisCredit"
