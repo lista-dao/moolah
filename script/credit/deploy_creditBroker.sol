c045d19b8bd217e4cc746b3a1585c777e25c142b
Razorback <raz.w@binance.com>
2026-02-03 16:08:37 +0800

    chore: change address to mainnet address

--- script/credit/deploy_creditBroker.sol
+++ script/credit/deploy_creditBroker.sol
@@ -7,13 +7,13 @@ import { CreditBroker } from "../../src/broker/CreditBroker.sol";
 
 contract DeployCreditBroker is Script {
   address moolah = 0x8F73b65B4caAf64FBA2aF91cC5D4a2A1318E5D8C;
-  address interestRelayer = address(0);
+  address interestRelayer = 0xBd94C4E931c1a15941B6273A952Af322891adC47;
   address oracle = 0xf3afD82A4071f272F403dC176916141f44E6c750;
   address lista = 0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46;
-  address creditToken = address(0);
+  address creditToken = 0x1f9831626CE85909794eEaA5C35BF34DB3eB52d8;
 
   function run() public {
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_TESTNET");
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     address deployer = vm.addr(deployerPrivateKey);
     console.log("Deployer: ", deployer);
     vm.startBroadcast(deployerPrivateKey);
--- script/credit/deploy_creditBrokerRelayer.sol
+++ script/credit/deploy_creditBrokerRelayer.sol
@@ -9,9 +9,10 @@ contract DeployBrokerInterestRelayer is Script {
   address vault = 0x4E82Fa869F8D05c8F94900d4652Fdb82f3C7A004;
   address moolah = 0x8F73b65B4caAf64FBA2aF91cC5D4a2A1318E5D8C;
   address _u = 0xcE24439F2D9C6a2289F741120FE202248B666666;
+  address lista = 0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46;
 
   function run() public {
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_TESTNET");
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     address deployer = vm.addr(deployerPrivateKey);
     console.log("Deployer: ", deployer);
     vm.startBroadcast(deployerPrivateKey);
@@ -23,7 +24,7 @@ contract DeployBrokerInterestRelayer is Script {
     // Deploy CreditBrokerInterestRelayer proxy
     ERC1967Proxy proxy = new ERC1967Proxy(
       address(impl),
-      abi.encodeWithSelector(impl.initialize.selector, deployer, deployer, moolah, vault, _u)
+      abi.encodeWithSelector(impl.initialize.selector, deployer, deployer, moolah, vault, _u, lista)
     );
     console.log("CreditBrokerInterestRelayer proxy: ", address(proxy));
 
