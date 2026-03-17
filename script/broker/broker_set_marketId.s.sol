c93cb5afb487e6dc75bd480912e5f633ff8fb53e
rick <jing.xin@lista.org>
2026-03-17 10:55:39 +0800

    fea: broker config scripts

--- script/broker/broker_set_marketId.s.sol
+++ script/broker/broker_set_marketId.s.sol
@@ -8,23 +8,9 @@ interface IBroker {
 }
 
 contract BrokerSetMarketIdScript is Script {
-  bytes32[] marketIds = [
-    bytes32(0x864a59352d12006ab1b194176c30b0e3f538e98baf78e9ee1c0d36e852727f77),
-    bytes32(0x3aeffa0dbe7aa8e3f3ae23c56f3aaf183af5f3736745a627e741cffb4ebfd6f3),
-    bytes32(0x4fe11b7007a4e09f1f274bfd152b636d7d64b4637df6b645c1516b05590797db),
-    bytes32(0xd6fe8c8658b8cc7e0f413b0e45e94646cc2ee9255e9500b0db0ee8c2c1499bff),
-    bytes32(0x212d0a36fccb86ff79994d6094271c21149c6a65e97e5ed797429ee56f44ce64),
-    bytes32(0x86e6bfa9e590d003ce03e34a79a4986120c4ced545ab62db484e43acb049c6a1)
-  ];
+  bytes32[] marketIds = [bytes32(0x226935103b730aefad53849e4cf7d92f30083cc417222f395478dabdd9ff3cac)];
 
-  address[] brokers = [
-    0x52ee1F685ef41E8D1158E2508dC46561Ca839864,
-    0xFDFc9A306084BCa33885b76d23C885dB9E3a6e72,
-    0x07b72Adbe196E2E83242C3414eee5Fd7E4c0cD74,
-    0x3350fC3c54CE501083a60707823833e67168bb94,
-    0xCA5929B8fF8B1a4B9B8d77DFc5340977BFa425B3,
-    0x306b7122adb734bD3976f6Fb7dC5E8fEf57528D7
-  ];
+  address[] brokers = [0x1Fa26015286D1270343d7526C60bd57aB6bE8b54];
 
   function run() public {
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
