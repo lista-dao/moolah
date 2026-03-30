b3f3ef7b2bb7b11e0e59f765c30f4035696c4074
rick <jing.xin@lista.org>
2026-03-30 14:09:18 +0800

    fea: add XAUT/USDC vault deploy and config scripts

---
+++ script/vaults/deploy_xaut_usdc_vault.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.8.34;
+
+import "forge-std/Script.sol";
+
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+import { MoolahVault } from "moolah-vault/MoolahVault.sol";
+
+contract DeployXautUsdcVault is Script {
+  address moolah = 0x8F73b65B4caAf64FBA2aF91cC5D4a2A1318E5D8C;
+
+  MoolahVault impl = MoolahVault(0x8F9475F2F5fEcccce21A14971DdE47498C2e51C3);
+
+  address XAUT = 0x21cAef8A43163Eea865baeE23b9C2E327696A3bf;
+  address USDC = 0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d;
+
+  function run() public {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address deployer = vm.addr(deployerPrivateKey);
+    console.log("Deployer: ", deployer);
+    vm.startBroadcast(deployerPrivateKey);
+
+    // Deploy XAUT Vault
+    ERC1967Proxy xautProxy = new ERC1967Proxy(
+      address(impl),
+      abi.encodeWithSelector(impl.initialize.selector, deployer, deployer, XAUT, "Lista XAUT Vault", "Lista XAUT Vault")
+    );
+    console.log("XAUT Vault proxy: ", address(xautProxy));
+
+    // Deploy USDC Vault
+    ERC1967Proxy usdcProxy = new ERC1967Proxy(
+      address(impl),
+      abi.encodeWithSelector(impl.initialize.selector, deployer, deployer, USDC, "Lista USDC Vault", "Lista USDC Vault")
+    );
+    console.log("USDC Vault proxy: ", address(usdcProxy));
+
+    vm.stopBroadcast();
+  }
+}
