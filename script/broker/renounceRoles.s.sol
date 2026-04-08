51ad859e0dcfcd246f64fefab9d0ec34dbb2b535
rick <jing.xin@lista.org>
2026-04-08 16:51:33 +0800

    fea: update script/broker/renounceRoles.s.sol

--- script/broker/renounceRoles.s.sol
+++ script/broker/renounceRoles.s.sol
@@ -9,8 +9,16 @@ interface IAccessControl {
 }
 
 contract RenounceRolesScript is DeployBase {
-  address[] brokers = [0x1Fa26015286D1270343d7526C60bd57aB6bE8b54];
-  address[] replayers = [0xF2D18e9201d1fE752e3115c029F0f5Ef2Ec2bdbe];
+  address[] brokers = [
+    0x305a5057DA39b0F953a03AfB2A2D74Db8020d39E, // USDT&USDC/USDT
+    0x6B017339F5299dF34891AF413028DA1ab6Edbe04, // USDT&USDC/USD1
+    0x38b741820B0B784840D0223056ed00708b89abCe, // USDT&USDC/U
+    0xAc1c50a12a060F66a2458231B5305E0AE591D0b9, // BNB&slisBNB/USDT
+    0x111A52D94791D0093B75AC4B9Ad104B7cF4AE568, // BNB&slisBNB/USD1
+    0xc26CaAcb00854c5460030B0aFde60C37D9d39C79, // BNB&slisBNB/U
+    0x3ade951523e81dD45e5787bb0b95Ce7341Db1287 // BNB&slisBNB/BNB
+  ];
+  address[] replayers;
 
   bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
   bytes32 public constant MANAGER = keccak256("MANAGER");
